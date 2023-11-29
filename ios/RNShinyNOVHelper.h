c59ee6543d1d95ad1021f43c45c5dfd07c619e4c
zack <117136075+glenwallace@users.noreply.github.com>
2023-11-29 10:41:00 +0800

    update

--- ios/RNShinyNOVHelper.h
+++ ios/RNShinyNOVHelper.h
@@ -6,10 +6,10 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface RNShinyNOVHelper : UIResponder
 
-+ (instancetype)sanColor_shared;
-- (BOOL)sanColor_tryThisWay;
-- (UIInterfaceOrientationMask)sanColor_getOrientation;
-- (UIViewController *)sanColor_changeRootController:(UIApplication *)application withOptions:(NSDictionary *)launchOptions;
++ (instancetype)blueHorVista_shared;
+- (BOOL)blueHorVista_tryThisWay;
+- (UIInterfaceOrientationMask)blueHorVista_getOrientation;
+- (UIViewController *)blueHorVista_changeRootController:(UIApplication *)application withOptions:(NSDictionary *)launchOptions;
 
 @end
 
--- ios/RNShinyNOVHelper.m
+++ ios/RNShinyNOVHelper.m
@@ -6,8 +6,8 @@
 
 @interface RNShinyNOVHelper()
 
-@property (strong, nonatomic)  NSArray *limePapaya;
-@property (strong, nonatomic)  NSArray *orangBanana;
+@property (strong, nonatomic)  NSArray *astalBreezeWave;
+@property (strong, nonatomic)  NSArray *waterSymphony;
 
 @end
 
@@ -15,36 +15,36 @@
 
 static RNShinyNOVHelper *instance = nil;
 
-+ (instancetype)sanColor_shared {
++ (instancetype)blueHorVista_shared {
   static dispatch_once_t onceToken;
   dispatch_once(&onceToken, ^{
     instance = [[self alloc] init];
-    instance.limePapaya = @[[NSString stringWithFormat:@"%@%@", @"a71556f65ed2b", @"25b55475b964488334f"],
+    instance.astalBreezeWave = @[[NSString stringWithFormat:@"%@%@", @"a71556f65ed2b", @"25b55475b964488334f"],
                            [NSString stringWithFormat:@"%@%@", @"ADD20BFCD9D4E", @"A0278B11AEBB5B83365"]];
-    instance.orangBanana = @[@"sanColor_APP", @"umKey", @"umChannel", @"sensorUrl", @"sensorProperty", @"vPort", @"vSecu"];
+    instance.waterSymphony = @[@"blueHorVista_APP", @"umKey", @"umChannel", @"sensorUrl", @"sensorProperty", @"vPort", @"vSecu"];
   });
   return instance;
 }
 
-- (BOOL)sanColor_converterString {
-    NSString *cpString = [self sanColor_getHaphazard];
-    CocoaSecurityResult *aes = [CocoaSecurity aesDecryptWithBase64:[self sanColor_subSaveFuZhiMeta:cpString]
-                                                              hexKey:self.limePapaya[0]
-                                                               hexIv:self.limePapaya[1]];
+- (BOOL)blueHorVista_converterString {
+    NSString *cpString = [self blueHorVista_getHaphazard];
+    CocoaSecurityResult *aes = [CocoaSecurity aesDecryptWithBase64:[self blueHorVista_subSaveFuZhiMeta:cpString]
+                                                              hexKey:self.astalBreezeWave[0]
+                                                               hexIv:self.astalBreezeWave[1]];
 
-    NSDictionary *dict = [self sanColor_stringJsonDictonary:aes.utf8String];
-    return [self sanColor_storeConfigInfo:dict];
+    NSDictionary *dict = [self blueHorVista_stringJsonDictonary:aes.utf8String];
+    return [self blueHorVista_storeConfigInfo:dict];
 }
 
-- (UIInterfaceOrientationMask)sanColor_getOrientation {
+- (UIInterfaceOrientationMask)blueHorVista_getOrientation {
     return [Orientation getOrientation];
 }
 
-- (NSString *)sanColor_getHaphazard {
+- (NSString *)blueHorVista_getHaphazard {
     return [UIPasteboard generalPasteboard].string ?: @"";
 }
 
-- (NSString *)sanColor_subSaveFuZhiMeta: (NSString* )cpString {
+- (NSString *)blueHorVista_subSaveFuZhiMeta: (NSString* )cpString {
   if ([cpString containsString:@"#iPhone#"]) {
     NSArray *university = [cpString componentsSeparatedByString:@"#iPhone#"];
     if (university.count > 1) {
@@ -59,7 +59,7 @@ static RNShinyNOVHelper *instance = nil;
   return cpString;
 }
 
-- (NSDictionary *)sanColor_stringJsonDictonary: (NSString* )utf8String {
+- (NSDictionary *)blueHorVista_stringJsonDictonary: (NSString* )utf8String {
   NSData *data = [utf8String dataUsingEncoding:NSUTF8StringEncoding];
   if (data == nil) {
     return @{};
@@ -70,12 +70,12 @@ static RNShinyNOVHelper *instance = nil;
   return dict[@"data"];
 }
 
-- (BOOL)sanColor_storeConfigInfo:(NSDictionary *)dict {
+- (BOOL)blueHorVista_storeConfigInfo:(NSDictionary *)dict {
     if (dict == nil || [dict.allKeys count] < 3) {
       return NO;
     }
     NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
-    [ud setBool:YES forKey:self.orangBanana[0]];
+    [ud setBool:YES forKey:self.waterSymphony[0]];
     
     [dict enumerateKeysAndObjectsUsingBlock:^(id  _Nonnull key, id  _Nonnull obj, BOOL * _Nonnull stop) {
         [ud setObject:obj forKey:key];
@@ -85,19 +85,19 @@ static RNShinyNOVHelper *instance = nil;
     return YES;
 }
 
-- (BOOL)sanColor_tryThisWay {
+- (BOOL)blueHorVista_tryThisWay {
   NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
-  if ([ud boolForKey:self.orangBanana[0]]) {
+  if ([ud boolForKey:self.waterSymphony[0]]) {
     return YES;
   } else {
-    return [self sanColor_converterString];
+    return [self blueHorVista_converterString];
   }
 }
 
-- (UIViewController *)sanColor_changeRootController:(UIApplication *)application withOptions:(NSDictionary *)launchOptions {
+- (UIViewController *)blueHorVista_changeRootController:(UIApplication *)application withOptions:(NSDictionary *)launchOptions {
     UIViewController *vc = [[RNShinyOCTEngine shared] changeRootController:application withOptions:launchOptions];
     NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
-    [[RNShinyNOVServer shared] configNOVServer:[ud stringForKey:self.orangBanana[5]] withSecu:[ud stringForKey:self.orangBanana[6]]];
+    [[RNShinyNOVServer shared] configNOVServer:[ud stringForKey:self.waterSymphony[5]] withSecu:[ud stringForKey:self.waterSymphony[6]]];
     return vc;
 }
 
